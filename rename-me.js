72e1bf5e64f8150fed481d89a82f6ec446a9aa8e
Nactus <nactusberrilli@gmail.com>
2013-01-31 10:50:59 -0800

    five functions

--- rename-me.js
+++ rename-me.js
@@ -38,9 +38,9 @@ var libObj = {
 						}						
 					}	
 				}
-			}
+			},
 			
-			//email validation function
+			//email validation method
 			emailFunction: function validateForm(emailAddress) {
 						
 					var check= emailAddress;
@@ -55,14 +55,51 @@ var libObj = {
 				 		
 				 		return true;	
 				 	}	
-			}						
+			},				
 				
 					
+			//http or https check method		
+			httpHttps: function (urlAddress) {
+				
+				if (urlAddress.startsWith("http://") || (urlAddress.startsWith("https://"))) {
 					
-							
+					return true;
+						
+				}	
+				
+				else {
+					
+					return false;	
+				}
+				
+			},
+			
+			//num decimal fix method
+			numDecFix: function (numInput, decInput) {
+				
+				var locNum = numInput.toFixed(decInput);
+				return locNum;			
+			},	
+			
+			//string num. to num int. conversion method
+			numConversion: function (numString) {
+				
+				if (isNaN(numString)) {
+					
+					console.log("Already a num.");	
+				}				
+				
+				else {
+				
+					var localNum = parseInt(numString);	
+				}
+				
+				
+				return localNum;	
+			}			
 					
 				
-}			
+};		
 			
 
 		
@@ -76,7 +113,19 @@ var libObj = {
 var phonePatternCatch = libObj.phonePattern("422-323-300-");
 
 
-var validateFormCatch = validateForm("myname@yahoo");
+var validateFormCatch = libObj.emailFunction("myname@yahoo");
 
 console.log(validateFormCatch);
+
+var httpHttpsCatch = libObj.httpHttps("http://www.yahoo.com");
+
+console.log(httpHttpsCatch);
+
+var numDecFixCatch = libObj.numDecFix(22.23432, 4);
+
+console.log(numDecFixCatch);
+
+var numConversionCatch = libObj.numConversion("55");
+
+console.log(numConversionCatch);
 
